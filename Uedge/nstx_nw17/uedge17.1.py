d266d5d83a80aa94eb4cee975ba6039cf760b63c
umansky <umansky1@llnl.gov>
2022-03-31 06:58:14 -0700

    Adding case uedge17.1.py and savefile uedge17.1.h5, minor edit in uedge18.0.py (added filter for ion force)

--- Uedge/nstx_nw17/uedge17.1.py
+++ Uedge/nstx_nw17/uedge17.1.py
@@ -1,27 +1,27 @@
-exec(open("uedge18.0.py").read())
+exec(open("uedge17.0.py").read())
 mu.paws("Base case set")
 
 exec(open("import_vorpal_data.py").read())
 mu.paws("Vorpal data imported")
 
 
-i=59
+i=100
 n=100
 ionFrac=i/n
 elFrac=i/n
 
 #-Note: minus sign to correct the bug in UEDGE
-#-Note: outer wall at x=0.30 m, antenna at y=-0.45 m
+#-Note: outer wall at x=0.40 m, antenna at y=-0.45 m
 bbb.pondomfpari_use[:,:,0]=-ionFrac*filter2d(pondomfpari_use, com.rm[:,:,0], com.zm[:,:,0], 
-                                         width=0.10, x0=0.30, y0=-0.45)
+                                         width=0.10, x0=0.40, y0=-0.45)
 
 bbb.pondomfpare_use[:,:]=elFrac*filter2d(pondomfpare_use, com.rm[:,:,0], com.zm[:,:,0], 
-                                         width=0.10, x0=0.30, y0=-0.45)
+                                         width=0.10, x0=0.40, y0=-0.45)
 
 
 #fname="./tmp_e"+ "{:.3f}".format(elFrac) + "_i" + "{:.3f}".format(ionFrac) + ".h5"
 #print("Restoring data from file ", fname)
-fname="uedge18.1.h5"
+fname="uedge17.1.h5"
 hdf5_restore(fname)
 
 bbb.dtreal=1e20
