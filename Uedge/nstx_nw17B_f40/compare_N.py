8f40e1180767a2f73446e9c8233de2f9497a0b6d
umansky <umansky1@llnl.gov>
2022-09-20 14:33:41 -0700

    Adding case nstx_nw18B_f40, updating nstx_nw18B_f58, nstx_nw17B_f30, nstx_nw17B_f40

--- Uedge/nstx_nw17B_f40/compare_N.py
+++ Uedge/nstx_nw17B_f40/compare_N.py
@@ -12,17 +12,17 @@ ufile="uedge17.2.h5"
 hdf5_restore(ufile)
 plt.plot(z, bbb.nis[:,21,0],"-.", label="2")
 
-ufile="uedge17.3.h5"
-hdf5_restore(ufile)
-plt.plot(z, bbb.nis[:,21,0],"-.", label="3")
+#ufile="uedge17.3.h5"
+#hdf5_restore(ufile)
+#plt.plot(z, bbb.nis[:,21,0],"-.", label="3")
 
-ufile="uedge17.4.h5"
-hdf5_restore(ufile)
-plt.plot(z, bbb.nis[:,21,0],"-.", label="4")
+#ufile="uedge17.4.h5"
+#hdf5_restore(ufile)
+#plt.plot(z, bbb.nis[:,21,0],"-.", label="4")
 
 plt.legend(loc="lower left")
 
-plt.title("case nstx_nw17B_f20", loc="right")
+plt.title("case nstx_nw17B_f40", loc="right")
 plt.suptitle("Ni along outer wall")
 plt.xlabel("R [m]")
 plt.ylabel("Ni [m-3]")
--- Uedge/nstx_nw18B_f40/compare_N.py
+++ Uedge/nstx_nw18B_f40/compare_N.py
@@ -8,9 +8,9 @@ ufile="uedge18.1.h5"
 hdf5_restore(ufile)
 plt.plot(z, bbb.nis[:,21,0],"--", label="1")
 
-ufile="uedge18.2.h5"
-hdf5_restore(ufile)
-plt.plot(z, bbb.nis[:,21,0],"-.", label="2")
+#ufile="uedge18.2.h5"
+#hdf5_restore(ufile)
+#plt.plot(z, bbb.nis[:,21,0],"-.", label="2")
 
 #ufile="uedge18.3.h5"
 #hdf5_restore(ufile)
@@ -18,7 +18,7 @@ plt.plot(z, bbb.nis[:,21,0],"-.", label="2")
 
 plt.legend(loc="lower left")
 
-plt.title("case nstx_nw18B_f58", loc="right")
+plt.title("case nstx_nw18B_f40", loc="right")
 plt.suptitle("Ni along outer wall")
 plt.xlabel("R [m]")
 plt.ylabel("Ni [m-3]")
